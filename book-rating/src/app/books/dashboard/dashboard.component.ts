1cfeb82ee9b62545d31387e75149800244e74234
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-20 14:55:50 +0100

    Books with interface

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { Book } from '../shared/book';
+import { JsonPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe, UpperCasePipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  books = ['AngularJS', 'Angular', 'jQuery'];
+  books: Book[] = [
+    {
+      isbn: '000',
+      title: 'Angular',
+      description: 'Super Buch',
+      rating: 5
+    },
+    {
+      isbn: '111',
+      title: 'AngularJS',
+      description: 'Altes Buch',
+      rating: 3
+    },
+    {
+      isbn: '222',
+      title: 'jQuery',
+      description: 'Gaaanz altes Buch',
+      rating: 1
+    }
+  ];
 
 }
