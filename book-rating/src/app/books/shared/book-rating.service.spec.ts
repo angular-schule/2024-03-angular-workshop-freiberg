77d9323750aa6dfdb9c3c500ad54e10fafb6e4dc
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-21 14:56:48 +0100

    testing BookRatingService

---
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BookRatingService } from './book-rating.service';
+import { Book } from './book';
+
+// ANG-1232: Bücher sollen wie bei Amazon bewertet werden
+describe('BookRatingService', () => {
+  let service: BookRatingService;
+  let book: Book;
+
+  beforeEach(() => {
+    service = new BookRatingService();
+    book = {
+      isbn: '000',
+      title: 'Test',
+      description: 'Test',
+      rating: 3
+    };
+  });
+
+  it('should rate up a book by one', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
+  });
+
+  it('should rate down a book by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('should not mutate the book', () => {
+    const frozenBook = Object.freeze(book);
+    expect(() => service.rateUp(frozenBook)).not.toThrow();
+    expect(() => service.rateDown(frozenBook)).not.toThrow();
+  })
+});
