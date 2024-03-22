d53e5d3547a8ac45b1a2fba1585c634bc53d2f62
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-22 11:11:10 +0100

    tests with spies (testing behaviour)

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,29 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+      imports: [
+        AppComponent,
+        // DashboardComponent // der alte Weg mit Modulen, Dashboard mit angeben, sonst kompiliert es nicht
+      ],
+    })
+    // .overrideComponent(AppComponent, {
+    //   set: { imports: [], schemas: [NO_ERRORS_SCHEMA] }
+    // })
+    .compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
 
-  it(`should have the 'book-rating' title`, () => {
+  it(`should have the 'Book Rating 😍' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title).toEqual('Book Rating 😍');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-rating');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating 😍');
   });
 });
--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const bookRatingMock = {
+      rateUp: (book: Book) => book,
+      // keine rateDown
+    };
+
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent],
+      providers: [{
+        provide: BookRatingService,
+        useValue: bookRatingMock
+      }]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('doRateUp() should call the BookRatingService', () => {
+
+    const rs = TestBed.inject(BookRatingService);
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    const book = {} as Book;
+    component.doRateUp(book);
+
+    expect(rs.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
